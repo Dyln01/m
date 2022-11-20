cdd83bcf93c74108ee2c57c300a7302d49faa10b
Dyln01 <92681447+Dyln01@users.noreply.github.com>
2022-11-20 21:44:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const http = require('http');
   
     const server = http.createServer(requestHandler)
   
-    server.listen(process.env.PORT, (err) => {  
+    server.listen(443, (err) => {  
       if (err) {
         return console.log('something bad happened', err)
       }
